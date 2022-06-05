098b64eec6a89bf455c3be60e8ceaaf6bda0e83e
Crownen187 <dennis.gerstung@stud.fra-uas.de>
2022-06-05 16:13:06 +0200

    main.py -> kommentare hinzugefügt

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import random
+import numpy
 from numpy import mean, sum
 
 
-class Conv:
+class Conv:                         #Random Number generator
     random_num = []
     n = random.sample(range(1, 1000), 5)
     random_num.append(n)
     print(random_num)
 
 
-class Log:
+class Log:                          #Random nummer von CONV wird in file gespeichert
     for num in range(len(Conv.random_num)):
         file = open("file.txt", "w")
         content = str(Conv.random_num)
@@ -21,12 +22,12 @@ class Log:
         file.close()
 
 
-class Stat:
+class Stat:                         #Start wertet die Daten aus
     sum = sum(Conv.random_num)
     avg = mean(Conv.random_num)
 
 
-class Report:
+class Report:                       #Report gibt die Daten wider
     print("\nThe sum of List is:\n", Stat.sum)
     print("\nThe average of List is:\n", Stat.avg)
 
