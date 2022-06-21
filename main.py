23116040915946c88c3eea40c15b67b63311d825
Alex03k <alex.kunkel457@gmail.com>
2022-06-21 19:55:14 +0200

    main.py -> Programm komplett überarbeitet

--- main.py
+++ main.py
@@ -1,77 +1,76 @@
+# This is a sample Python script.
 import random
-import threading
-import numpy
-from  queue import  Queue
+import time
 from numpy import mean, sum
-
-
-class Conv:
-    random_num = []
-    n = random.sample(range(1, 1000), 5)
-    random_num.append(n)
-    print(random_num)
-
-
-
-class Log:
-    for num in range(len(Conv.random_num)):
-        file = open("file.txt", "w")
-        content = str(Conv.random_num)
-        file.write(content)
-        file.close()
-        file = open("file.txt", "r")
-        content = file.read()
-        print("\nContent in file.txt:\n", content)
-        file.close()
-
-
-class Stat:
-    sum = sum(Conv.random_num)
-    avg = mean(Conv.random_num)
-
-
-class Report:
-    print("\nThe sum of List is:\n", Stat.sum)
-    print("\nThe average of List is:\n", Stat.avg)
-
-
-
-
-
-
-
 
 
 class Queue:
 
-	def __init__(self):
-		self.elements = []
+    def __init__(self):
+        self.elements = []
 
-	def enqueue(self, data):
-		self.elements.append(data)
-		return data
+    def enqueue(self, data):
+        self.elements.append(data)
+        return data
 
-	def dequeue(self):
-		return self.elements.pop(0)
+    def dequeue(self):
+        return self.elements.pop(0)
 
-	def rear(self):
-		return self.elements[-1]
+    def rear(self):
+        return self.elements[-1]
 
-	def front(self):
-		return self.elements[0]
+    def front(self):
+        return self.elements[0]
 
-	def is_empty(self):
-		return len(self.elements) == 0
+    def is_empty(self):
+        return len(self.elements) == 0
+
 
 if __name__ == '__main__':
     queue = Queue()
+    queue2 = Queue()
 
-    i=0
+while True:
+    ## Conv:
+    while True:
+        queue.enqueue(random.sample(range(1, 1000), 1))
+        print(len(queue.elements))
+        if len(queue.elements) > 9:
+            break
 
-    länge=len(Conv.random_num)
-    print(länge)
-    for i in range(i,5,1):
-        print(i)
-        queue.enqueue(Conv.random_num[i])
+    ## Log:
+    content = []
+    content = queue.elements
+    print(content)
+    for num in range(len(queue.elements)):
+        file = open("file2.txt", "a")
+        content = queue.dequeue()
+        file.write(str(content))
+        file.close()
+        print("\nContent in file2.txt:\n", content)
+        file.close()
+        queue.enqueue(content)
+
+    ## Stat:
+    summe = sum(queue.elements)
+    avg = summe / len(queue.elements)
+    queue2.enqueue(summe)
+    queue2.enqueue(avg)
+
+    ## Report:
+    print("Die Summe berägt: ", queue2.dequeue())
+    print("Der Mittelwert beträgt", queue2.dequeue())
+    ## Queue neustart
+    while not queue.is_empty():
+        queue.dequeue()
+
+    time.sleep(5)
+
+
+
+
+
+
+
 
 
