e2d186c82e7875ccaa4099c04b2b5154401b4e88
Alex03k <alex.kunkel457@gmail.com>
2022-06-23 16:37:55 +0200

    main.py -> Programm nochmal komplett überarbeitet

--- main.py
+++ main.py
@@ -1,76 +1,58 @@
-# This is a sample Python script.
+import os
 import random
 import time
-from numpy import mean, sum
+from multiprocessing import Queue
 
+q = Queue(maxsize=10)
+q2 = Queue(maxsize=10)
+q3 = Queue()
+q4 = Queue()
+pid = os.fork()
+if pid < 0:
+    print("fork hat nicht funktioniert")
 
-class Queue:
+if pid > 0:
 
-    def __init__(self):
-        self.elements = []
+    pid2 = os.fork()
 
-    def enqueue(self, data):
-        self.elements.append(data)
-        return data
-
-    def dequeue(self):
-        return self.elements.pop(0)
-
-    def rear(self):
-        return self.elements[-1]
-
-    def front(self):
-        return self.elements[0]
-
-    def is_empty(self):
-        return len(self.elements) == 0
-
-
-if __name__ == '__main__':
-    queue = Queue()
-    queue2 = Queue()
-
-while True:
-    ## Conv:
+    if pid2 > 0:
+        print("Elternprozess")
     while True:
-        queue.enqueue(random.sample(range(1, 1000), 1))
-        print(len(queue.elements))
-        if len(queue.elements) > 9:
+        n = random.sample(range(1, 1000), 1)
+        q.put(n)
+        q2.put(n)
+        if q.full():
             break
 
-    ## Log:
-    content = []
-    content = queue.elements
-    print(content)
-    for num in range(len(queue.elements)):
-        file = open("file2.txt", "a")
-        content = queue.dequeue()
-        file.write(str(content))
-        file.close()
-        print("\nContent in file2.txt:\n", content)
-        file.close()
-        queue.enqueue(content)
+    if pid2 == 0:
+        time.sleep(5)
+        print("Child2")
+        for num in range(q.qsize()):
+            content = q.get(num)
+            file = open("file2.txt", "w")
+            file.write(str(content))
+            file.close()
+            print("\nContent in file2.txt:\n", content)
 
-    ## Stat:
-    summe = sum(queue.elements)
-    avg = summe / len(queue.elements)
-    queue2.enqueue(summe)
-    queue2.enqueue(avg)
+if pid == 0:
+    pid3 = os.fork()
+    if pid3 > 0:
+        time.sleep(10)
+        print("Child1")
 
-    ## Report:
-    print("Die Summe berägt: ", queue2.dequeue())
-    print("Der Mittelwert beträgt", queue2.dequeue())
-    ## Queue neustart
-    while not queue.is_empty():
-        queue.dequeue()
+        print("q2size", q2.qsize())
+        durch = int(q2.qsize())
+        numbers = []
+        for i in range(q2.qsize()):
+            verarbeiten = str(q2.get(i))
+            numbers.append(int(verarbeiten.strip('[]')))
+        summe = sum(numbers)
+        mittelWert = (summe / durch)
+        q3.put(summe)
+        q4.put(mittelWert)
 
-    time.sleep(5)
-
-
-
-
-
-
-
-
-
+    if pid3 == 0:
+        time.sleep(15)
+        print("Child from Child")
+        print("Summe", q3.get())
+        print("Mittelwert", q4.get())
