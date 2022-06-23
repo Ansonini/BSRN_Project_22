58e286fe2f3a070b920e48863c13fa731265bb74
Lucian <lucian-zimmermann@web.de>
2022-06-23 18:13:37 +0100

    shared_memory_semaphore.c function erstellen und einfügen, conv und stat funktionieren, log error bei file erstellen und report noch nicht fertig

--- shared_memory_semaphore.c
+++ shared_memory_semaphore.c
@@ -1,6 +1,49 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    printf("Hello World!");
+void conv(num[]);
+
+void log(num[]);
+
+void stat(num[]);
+
+void report();
+
+int main() {
+    int num[10];
+    conv(num);
+ //   log(num);
     return 0;
+}
+
+void conv(num[]) {
+
+    for (int i = 1; i <= 10; i++) {
+        num = rand() % 1000 + 1;
+        printf("%d\n", num);
+    }
+}
+
+void log(num[]) {
+   /* FILE *fp = NULL;
+
+    fp = fopen("log_C.txt", "w");
+
+    if (fp != NULL) {
+        for (int i = 0; i <= 10; i++) {
+            fprintf("%d \n", num[i]);
+        }
+    } */
+}
+
+void stat(num[]) {
+int sum, med;
+    for (int i = 0; i < num; i++) {
+        sum += num[i];
+    }
+    med = sum / 10;
+}
+
+void report() {
+
 }
