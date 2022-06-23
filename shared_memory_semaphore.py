c287d717ef4926d1d5a08d904a215f8ba1fac82f
Lucian <lucian-zimmermann@web.de>
2022-06-23 21:16:09 +0100

    Start shared_memory_semaphore.py -> Introducing Forc() processes

--- shared_memory_semaphore.py
+++ shared_memory_semaphore.py
@@ -1,5 +1,30 @@
+import os
+import multiprocessing
 import threading
+import random
+import time
 
-semaphore = threading.Semaphore
+PROZESS_ID = os.fork()
 
-semaphore.acquire()
+
+if PROZESS_ID != 0:
+
+    PROZESS_ID2 = os.fork()
+
+    if PROZESS_ID2 != 0:
+        print("Parent")
+
+
+    if PROZESS_ID2 == 0:
+        print("Child2")
+
+
+if PROZESS_ID == 0:
+
+    PROZESS_ID3 = os.fork()
+    if PROZESS_ID3 != 0:
+        print("Child1")
+
+
+    if PROZESS_ID3 == 0:
+        print("Child from Child")
