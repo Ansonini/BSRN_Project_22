00dcb89c4ed6916f0f915c11dbb059221e0ea30d
Lucian <lucian-zimmermann@web.de>
2022-06-23 22:14:27 +0100

    Start shared_memory_semaphore.py -> Fixing up the If statements

--- shared_memory_semaphore.py
+++ shared_memory_semaphore.py
@@ -4,27 +4,21 @@ import threading
 import random
 import time
 
-PROZESS_ID = os.fork()
-
+PROCESS_ID = os.fork()
 
 if PROZESS_ID != 0:
 
-    PROZESS_ID2 = os.fork()
+    PROCESS_ID2 = os.fork()
 
     if PROZESS_ID2 != 0:
         print("Parent")
-
-
-    if PROZESS_ID2 == 0:
+    else:  # PROCESS_ID2 == 0
         print("Child2")
 
+else:  # PROCESS_ID == 0
 
-if PROZESS_ID == 0:
-
-    PROZESS_ID3 = os.fork()
+    PROCESS_ID3 = os.fork()
     if PROZESS_ID3 != 0:
         print("Child1")
-
-
-    if PROZESS_ID3 == 0:
+    else:  # PROCESS_ID3 == 0
         print("Child from Child")
