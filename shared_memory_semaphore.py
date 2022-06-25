803ec16ba81ed42a844902c8407e0399b3a78396
Lucian <lucian-zimmermann@web.de>
2022-06-25 22:42:15 +0100

    shared_memo_test_weil_ich_bin_dumm.py -> Print leerzeichen zur überschaubarkeit hinzugefügt
    shared_memory_semaphore.py -> In fork unterteilt

--- shared_memory_semaphore.py
+++ shared_memory_semaphore.py
@@ -22,22 +22,13 @@ if PROZESS_ID != 0:
     if PROZESS_ID2 != 0:
         print("Parent")
         # Creating a random number while the Semaphore allows it writing it into the shared memory block
-        while True:
-            sema.acquire()
-            num = (random.sample(range(1, 1000), 1))
-            shared_mem_1.buf[0] = num
-            sema.release()
+        #Conv
 
     # Child 2
     else:  # PROCESS_ID2 == 0
         print("Child2")
         # Taking the num out of the memory block and putting it into the file
-        while True:
-            sema.acquire()
-            file = open("file_SHM_S.txt", "w")
-            file.write(shared_mem_1[0])
-            file.close()
-            sema.release()
+        #Log
 
 # Child1 Process
 
@@ -50,25 +41,15 @@ else:  # PROCESS_ID == 0
     # Child1 (now Parent from Child PROZESS_ID3)
     if PROZESS_ID3 != 0:
         print ("Child1 (now Parent of Child_of_Child")
-        while True:
-            sema.acquire()
-            num_list = []
-            num_list.append(shared_memory[0])
-            sum_num = sum(num_list)
-            mean_num = sum_num / len(sum_num)
-            shared_mem_1[0] = str(sum_num)
-            shared_mem_1[1] = str(mean_num)
-            sema.release()
+        #STAT
 
     #Child of Child1
     else:  # PROCESS_ID3 == 0
         print("Child from Child")
 
+        #REPORT
+
         # Print Mean from report
-        while True:
-            print("Child1")
+        print("Child1")
             # Print Sum and Mean from report
-            sema.acquire()
-            print("The Sum is : " + shared_mem_1[0])
-            print("The Mean is : " + shared_mem_1[1])
-            sema.release()
+
