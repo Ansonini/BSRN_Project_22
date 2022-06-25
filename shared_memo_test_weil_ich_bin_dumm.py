b1b881f6f4eb30c5cad992c2ac3fac69783494ab
Lucian <lucian-zimmermann@web.de>
2022-06-25 13:09:30 +0100

    simple_test.py -> test

--- shared_memo_test_weil_ich_bin_dumm.py
+++ shared_memo_test_weil_ich_bin_dumm.py
@@ -1,50 +1,21 @@
-import os
 from multiprocessing import shared_memory
 from threading import Semaphore, BoundedSemaphore
 import random
 import time
 
+# Creating the Shared Memory with a Byte size of 10
+shared_mem_1 = shared_memory.SharedMemory(create=True, size=10)
 # Creating my Semaphore
 sema = Semaphore(1)
-# Creating the Shared Memory with a Byte size of 10
-shared_mem_1 = shared_memory.SharedMemory(name='shared_memory', create=True, size=10)
+
 
 
 # Creating a random number while the Semaphore allows it writing it into the shared memory block
 while True:
     sema.acquire()
-    num = (random.sample(range(1, 1000), 1))
-    shared_mem_1.buf[0] = num
-    sema.release()
-
-# Taking the num out of the memory block and putting it into the file
-while True:
-    sema.acquire()
-    file = open("file_SHM_S.txt", "w")
-    file.write(shared_mem_1[0])
-    file.close()
-    sema.release()
-
-# Creating a ShadeMemory List appending all the num generated from Conv
-while True:
-    sema.acquire()
-    num_list = []
-    num_list.append(shared_memory[0])
-    sum_num = sum(num_list)
-    mean_num = sum_num / len(sum_num)
-    shared_mem_1[0] = str(sum_num)
-    shared_mem_1[1] = str(mean_num)
-    sema.release()
-
-
-# Print Sum from report
-while True:
-    sema.acquire()
-    print("The Sum is : " + shared_mem_1[0])
-    sema.release()
-
-    # Print Mean from report
-while True:
-    sema.acquire()
-    print("The Mean is : " + shared_mem_1[1])
+    num = (random.sample(range(1, 1000), 10))
+    print("num", num)
+    for i, c in enumerate(b"Hello world"):
+        shared_mem_1.buf[i] = c
+    print(shared_mem_1.buf[0])
     sema.release()
---
+++ simple_test.py
@@ -0,0 +1,16 @@
+from multiprocessing import shared_memory
+
+shm_a = shared_memory.SharedMemory(name= 'shared_memory', create= True, size= 12)
+
+for i,c in enumerate(b"Hello world"):
+    shm_a.buf[i] = c
+
+while True:
+    try:
+        pass
+    except KeyboardInterrupt:
+        break
+
+print(bytes(shm_a.buf[:12]))
+shm_a.close()
+shm_a.unlink()
