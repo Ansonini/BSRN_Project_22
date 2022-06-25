945a9e67ea375093925018f0db47c69a70e543c1
Lucian <lucian-zimmermann@web.de>
2022-06-25 17:06:56 +0100

    simple_test_other_shared_memo.py -> deleted simple_test.py -> deleted shared_memo_test_weil_ich_bin_dumm.py -> übernommen

--- shared_memo_test_weil_ich_bin_dumm.py
+++ shared_memo_test_weil_ich_bin_dumm.py
@@ -4,17 +4,24 @@ import random
 import time
 
 # Creating the Shared Memory with a Byte size of 10
-shared_mem_1 = shared_memory.SharedMemory(create=True, size=10)
+shared_mem_1 = shared_memory.SharedMemory(create=True, size=100)
 # Creating my Semaphore
 sema = Semaphore(1)
-
 
 
 # Creating a random number while the Semaphore allows it writing it into the shared memory block
 while True:
     sema.acquire()
-    num = (random.sample(range(1, 250), 1))
+    num = int
+    num = str(((random.sample(range(1, 255), 1))))
+    verarbeiten = str(num)
+    nummer = int(verarbeiten.strip('[]'))
+
+    print(type(num),)
     print("num", num)
-    shared_mem_1.buf[0] = num
-    print(shared_mem_1.buf[0])
+    print(type(nummer), nummer)
+    b =bytes(1)
+    for i, c in enumerate(b, nummer):
+        shared_mem_1.buf[0] = nummer
+        print(shared_mem_1.buf[0])
     sema.release()
