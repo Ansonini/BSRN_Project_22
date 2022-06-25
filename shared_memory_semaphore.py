92eabbde9ca4bfd19bb735eca40022c96b52464f
Lucian <lucian-zimmermann@web.de>
2022-06-25 12:53:07 +0100

    Start shared_memory_semaphore.py -> Probiere shared_memory zu benutzen
    shared_memo_test_weil_ich_bin_dumm.py -> eine kurzzeitige demo test classe weil ich es nicht geschissen bekommen :(

--- shared_memory_semaphore.py
+++ shared_memory_semaphore.py
@@ -7,21 +7,21 @@ import time
 # Creating my Semaphore
 sema = Semaphore(1)
 # Create Parent process ID
-PROCESS_ID = os.fork()
+PROZESS_ID = os.fork()
 # Creating the Shared Memory with a Byte size of 10
 shared_mem_1 = shared_memory.SharedMemory(name='shared_memory', create=True, size=10)
 
 if PROZESS_ID != 0:
 
-    PROCESS_ID2 = os.fork()
+    PROZESS_ID2 = os.fork()
 
     if PROZESS_ID2 != 0:
         print("Parent")
         # Creating a random number while the Semaphore allows it writing it into the shared memory block
         while True:
             sema.acquire()
-            num = random.sample(range(1, 1000), 1)
-            shared_mem_1[0] = num
+            num = (random.sample(range(1, 1000), 1))
+            shared_mem_1.buf[0] = num
             sema.release()
 
     else:  # PROCESS_ID2 == 0
@@ -43,11 +43,11 @@ else:  # PROCESS_ID == 0
         num_list.append(shared_memory[0])
         sum_num = sum(num_list)
         mean_num = sum_num / len(sum_num)
-        shared_mem_1[0] = sum_num
-        shared_mem_1[1] = mean_num
+        shared_mem_1[0] = str(sum_num)
+        shared_mem_1[1] = str(mean_num)
         sema.release()
 
-    PROCESS_ID3 = os.fork()
+    PROZESS_ID3 = os.fork()
     if PROZESS_ID3 != 0:
         print("Child1")
         # Print Sum from report
