b5f8d2158ec5e6095a19faf1884ce5f55f48b36a
Lucian <lucian-zimmermann@web.de>
2022-06-25 03:32:04 +0100

    Start shared_memory_semaphore.py -> Changing the Semaphores and adding them to the steps from shared memory

--- shared_memory_semaphore.py
+++ shared_memory_semaphore.py
@@ -4,10 +4,10 @@ from threading import Semaphore, BoundedSemaphore
 import random
 import time
 
+# Creating my Semaphore
+sema = Semaphore(1)
 # Create Parent process ID
 PROCESS_ID = os.fork()
-# Creating my Semaphore
-sema = Semaphore
 # Creating the Shared Memory with a Byte size of 10
 shared_mem_1 = shared_memory.SharedMemory(name='shared_memory', create=True, size=10)
 
@@ -18,34 +18,48 @@ if PROZESS_ID != 0:
     if PROZESS_ID2 != 0:
         print("Parent")
         # Creating a random number while the Semaphore allows it writing it into the shared memory block
-        while sema <= 0:
+        while True:
+            sema.acquire()
             num = random.sample(range(1, 1000), 1)
             shared_mem_1[0] = num
+            sema.release()
 
     else:  # PROCESS_ID2 == 0
         print("Child2")
         # Taking the num out of the memory block and putting it into the file
-        file = open("file_SHM_S.txt", "w")
-        file.write(shared_mem_1[0])
-        file.close()
+        while True:
+            sema.acquire()
+            file = open("file_SHM_S.txt", "w")
+            file.write(shared_mem_1[0])
+            file.close()
+            sema.release()
 
 # Child of the first process
 else:  # PROCESS_ID == 0
     # Creating a ShadeMemory List appending all the num generated from Conv
-    num_list = []
-    num_list.append(shared_memory[0])
-    sum_num = sum(num_list)
-    mean_num  = sum_num/len(sum_num)
-    shared_mem_1[0] = sum_num
-    shared_mem_1[1] = mean_num
+    while True:
+        sema.acquire()
+        num_list = []
+        num_list.append(shared_memory[0])
+        sum_num = sum(num_list)
+        mean_num = sum_num / len(sum_num)
+        shared_mem_1[0] = sum_num
+        shared_mem_1[1] = mean_num
+        sema.release()
 
     PROCESS_ID3 = os.fork()
     if PROZESS_ID3 != 0:
         print("Child1")
         # Print Sum from report
-        print("The Sum is : " + shared_mem_1[0])
+        while True:
+            sema.acquire()
+            print("The Sum is : " + shared_mem_1[0])
+            sema.release()
 
     else:  # PROCESS_ID3 == 0
-        # Print Mean from report
-        print("The Mean is : " + shared_mem_1[1])
         print("Child from Child")
+        # Print Mean from report
+        while True:
+            sema.acquire()
+            print("The Mean is : " + shared_mem_1[1])
+            sema.release()
